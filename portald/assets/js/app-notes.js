74079b8b4f0c8fae6cb4cbc5323d4a78f5dfa4bb
ekaxis <duyqajk@gmail.com>
2021-04-14 00:13:50 +0000

    bugfix: django.core.exceptions.ImproperlyConfigured: Cannot import 'public'. Check that 'apps.public.apps.PublicConfig.name' is correct.

--- portald/assets/js/app-notes.js
+++ portald/assets/js/app-notes.js
@@ -83,6 +83,8 @@ $(document).ready(function() {
                 classDiv.add('note-'+topicNote);
 
                 divNote.attributes['data-title'].nodeValue = titleNote;
+                divNote.attributes['data-text'].nodeValue = textNote;
+                divNote.attributes['data-topic'].nodeValue = topicNote;
 
                 console.log(divNote);
                 console.log(pTitle);
@@ -288,6 +290,7 @@ $(document).ready(function() {
                         '<div class="note-action">' +
                             '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star fav-note"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg> ' +
                             '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 delete-note"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>' +
+                            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit edit-note"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>' +
                         '</div>' +
                         '<div class="note-footer">' +
                             '<div class="tags-selector btn-group">' +
